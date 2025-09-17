8980f9cd8623e8e960d7a175fcb424036c05b40a
CalebS0 <4601424@spsd.sk.ca>
2025-09-17 14:10:32 -0600

    Spetmeber

--- Assignments/01 Interactive Scene/sketch.js
+++ Assignments/01 Interactive Scene/sketch.js
@@ -5,25 +5,58 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let skyred = 50
+let skygreen = 200
+let skyblue = 255
+let suncolor = 1
 
 function setup() {
   createCanvas(600,600);
 }
 
 function draw() {
-  background(50, 200, 255);
+  background(50, 200-(mouseY/4), 255-(mouseY/4));
+  clouds()
   sun()
-  mountains()
+  pyramid()
+  noStroke()
 }
-
+function mousePressed() {
+  if (mouseButton === LEFT && mouseIsPressed) {
+    if (suncolor >= 3) {
+      suncolor = 1
+    }
+   suncolor += 1
+  }
+}
 function sun() {
-  fill(255, 200, 0)
+  if (suncolor === 1) {
+    fill(225, 200, 0)
+  }
+  else if (suncolor === 2) {
+    fill(225, 100, 0)
+  }
   circle(mouseX, mouseY, 100)
 }
  
-function mountains() {
-  fill(220, 220, 100)
+function pyramid() {
+  fill(400-(mouseX/2), 400-(mouseX/2), 125-(mouseX/4))
 triangle((width/2), (height/2.5), 400, 600, 0, 600 )
-fill(mouseX/1.5, mouseX/1.5, mouseX/4)
+fill(50+(mouseX/2.5), 50+(mouseX/2.5), mouseX/4)
 triangle(width/2, height/2.5, 400, 600, 600, 600)
+}
+function clouds() {
+  fill("white")
+  circle(400, 125, 75) //Each Cloud
+  circle(450, 150, 75)
+  circle(500, 135, 75)
+  circle(475, 100, 75)
+  circle(425, 100, 75)
+
+  circle(100, 150, 75) //Each Cloud
+  circle(150, 175, 75)
+  circle(200, 160, 75)
+  circle(175, 125, 75)
+  circle(125, 125, 75)
+  
 }

577cf1779dbc96a6c18d742170aa8c5061e56d6c
CalebS0 <4601424@spsd.sk.ca>
2025-10-10 11:29:18 -0600

    october

---
+++ demos/14 Objects Mini-demo/sketch.js
@@ -0,0 +1,54 @@
+// Objects Mini-demo
+// Caleb Schwab
+// 10/10/25
+
+// Globals ------ "simple data only"
+
+let myBall;
+let ballCollection = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  myBall = new Ball(100,100);
+}
+
+function draw() {
+  background(220);
+  // process SINGLE Ball object
+  myBall.move();
+  myBall.display();
+
+  // add new objects to array
+  if(mouseIsPressed){
+    ballCollection.push(new Ball(mouseX, mouseY));
+  }
+
+  // process COLLECTION of objects
+  for(let b of ballCollection){
+    b.move();
+    b.display();
+  }
+}
+
+class Ball{
+  // Constructor
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.c = color(random(255),random(255),random(255));
+    this.size = 15;
+    this.speed = random(2,10);
+  }
+
+  // Class Methods/Functions
+  display(){
+    fill(this.c);
+    circle(this.x, this.y, this.size);
+  }
+
+  move(){
+    // horizontal movement only
+    this. x += this.speed;
+    if(this.x > width) this.x = 0;
+  }
+}
